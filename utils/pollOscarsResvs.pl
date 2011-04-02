7b99bfcb7cdc0c77e4dc4edced99c2e123699430
xyang <xyang@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2011-04-02 15:55:51 +0000

    pollOscarsResvs.pl utility
    git-svn-id: file:///var/www/svn/mxtce-sw@90 52860be4-34ab-47e5-b986-9ec2afffd7f9

--- utils/pollOscarsResvs.pl
+++ utils/pollOscarsResvs.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Lib::TLV;
 use Lib::APIClient;
+use Time::ParseDate;
 
 
 BEGIN {
@@ -21,6 +22,41 @@ use constant MSG_REQ => 0x0001;
 use constant MSG_TLV_RESV_INFO => 0x0011;
 use constant MSG_TLV_PATH_ELEM => 0x0012;
 
+sub parse_resv_list($)
+{
+	my $text = shift;
+	my @resvs;
+	foreach my $line (split(/\n/, $text)) {
+		my $resv = $resvs[-1];
+		if ($line =~ /GRI:\s([^\s]+)/) {
+			my %resvMem;
+			$resv = \%resvMem;
+			$resv->{gri} = $1;
+			push(@resvs, $resv);
+		} elsif ($line =~ /Status:\s([^\s]+)/) {
+			$resv->{status} = $1;
+		} elsif ($line =~ /startTime:\s([^\s])+\s([^\s]+\s[^\s]+\s[^\s]+\s[^\s]+\s[^\s]+)/) {
+		my $start = parsedate($resv->{start});
+			$resv->{start} = parsedate($2);
+		} elsif ($line =~ /endTime:\s([^\s])+\s([^\s]+\s[^\s]+\s[^\s]+\s[^\s]+\s[^\s]+)/) {
+			$resv->{end} = parsedate($2);
+		} elsif ($line =~ /bandwidth:\s([^\s]+)/) {
+			$resv->{bw} = $1;
+		} elsif ($line =~ /(urn:ogf:[^\s]+)\s[^\s]+\s([^\s]+)/) {
+			if (!defined($resv->{path})) {
+				my @path;
+				$resv->{path} = \@path;
+			}
+			my %elem;
+			$elem{urn} = $1;
+			$elem{swtype} = 51;
+			$elem{enc} = 2;
+			$elem{vlan} = $2;
+			push(@{$resv->{path}}, \%elem);
+		}
+	}
+	return @resvs;
+}
 
 die 'Abort: env variable $OSCARS_DIST undefined...' unless (defined($ENV{OSCARS_DIST}));
 
@@ -31,18 +67,22 @@ my $api_conn = new APIClient('localhost', '2091', 10101);
 
 while(1) {
 	my $resv_list_text = `$oscars_cmd`;
-	my @resvs;
-	#TODO: parse text into @resvs (each a \%resv={gri, status, start, end, bw, \@path}
+	#parse text into @resvs (each a \%resv={gri, status, start, end, bw, \@path}
+	my @resvs = parse_resv_list($resv_list_text);
 	foreach my $resv (@resvs) {
+		#TODO: skip resv of unqualified status
 		my $resv_info_tlv = new TLV(MSG_TLV_RESV_INFO, $resv->{gri}, $resv->{start}, $resv->{end}, $resv->{bw}, $resv->{status}); 
 		my @path_elem_tlvs;
-		foreach my $elem ($resv->{path}) {
-			my $elem_tlv = new tlV(MSG_TLV_PATH_ELEM, $elem->{urn}, $elem->{swtype}, $elem->{enc}, $elem->{vlan});
+		foreach my $elem (@{$resv->{path}}) {
+			my $elem_tlv = new TLV(MSG_TLV_PATH_ELEM, $elem->{urn}, $elem->{swtype}, $elem->{enc}, $elem->{vlan});
 			push(@path_elem_tlvs, $elem_tlv);
 		}
                 $api_conn->queue_bin_msg(MSG_REQ, 0, $resv_info_tlv, @path_elem_tlvs);
                 $api_conn->send_bin_msg();
+		print %{$resv};
+		print  "\n";
 	}
+	print "sleep $interval seconds\n";
 	sleep($interval);
 }
 
