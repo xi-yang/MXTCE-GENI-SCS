c2fe3c594b83f71ac2d9a3e193128281d0cc8afb
xyang <xyang@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2011-09-14 01:42:33 +0000

    Reservation poll script for OSCARS v5
    git-svn-id: file:///var/www/svn/mxtce-sw@155 52860be4-34ab-47e5-b986-9ec2afffd7f9

---
+++ utils/pollOscarsResvsV5.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Lib::TLV;
+use Lib::APIClient;
+use DBI;
+
+use constant API_MSG_RESV_PUSH => 0x0011;
+use constant MSG_TLV_RESV_INFO => 0x0011;
+use constant MSG_TLV_PATH_ELEM => 0x0012;
+
+my $host = shift;
+
+$host = 'localhost' unless defined($host);
+
+my $dbh = DBI->connect('dbi:mysql:bss','oscars','oscars') or die "Connection Error: $DBI::errstr\n";
+
+my $sql = "select R.globalReservationId,R.status,R.startTime,R.endTime,R.bandwidth,E.urn,V.swcap,V.value from reservations as R,paths as P,pathElems as E,pathElemParams as V where (R.status='ACTIVE' or R.status='PENDING' or R.status='INSETUP') and P.pathType='local' and V.type='suggestedVlan' and P.reservationId=R.id and E.pathId=P.id and V.pathElemId=E.id order by R.id, E.id";
+
+my $interval = 120;
+
+my $api_conn = new APIClient($host, '2091', 10101);
+$api_conn->connect_server();
+
+sub parse_resvs($)
+{
+    my $sth = shift;
+    my @resvs;
+    while (my @row = $sth->fetchrow_array) {
+        my $resv = $resvs[-1];
+        if (!defined($resv) || $resv->{gri} ne $row[0]) {
+            my %resvMem;
+            $resv = \%resvMem;
+            $resv->{gri} = $row[0];
+            $resv->{status} = $row[1];
+            $resv->{start} = $row[2];
+            $resv->{end} = $row[3];
+            $resv->{bw} = $row[4];
+            push(@resvs, $resv);
+        }
+        if (!defined($resv->{path})) {
+            my @path;
+            $resv->{path} = \@path;
+        }
+
+        my %elemMem;
+        my $elem = \%elemMem;
+        $elem->{urn} = $row[5];
+        $elem->{swtype} = 51;
+        $elem->{enc} = 2;
+        $elem->{vlan} = $row[7];
+        if ($row[7] eq "null") {
+            $elem->{vlan} = 0;
+        }
+        push(@{$resv->{path}}, $elem);
+    }
+    return @resvs;
+}
+
+while(1) {
+    my $sth = $dbh->prepare($sql);
+    $sth->execute or die "SQL Error: $DBI::errstr\n";
+    my @resvs = parse_resvs($sth);
+    my $ct = 1;
+    foreach my $resv (@resvs) {
+            my $resv_info_tlv = new TLV(MSG_TLV_RESV_INFO, $resv->{gri}, $resv->{start}, $resv->{end}, $resv->{bw}, $resv->{status});
+            my @path_elem_tlvs;
+            foreach my $elem (@{$resv->{path}}) {
+                    my $elem_tlv = new TLV(MSG_TLV_PATH_ELEM, $elem->{urn}, $elem->{swtype}, $elem->{enc}, $elem->{vlan});
+                    push(@path_elem_tlvs, $elem_tlv);
+            }
+            my $opts = 0;
+            # indicate start of update burst
+            if ($ct == 1) {
+                    $opts = ($opts | 0x0001);
+            }
+            # indicate end of update burst
+            if ($ct == $#resvs) {
+                    $opts = ($opts | 0x0002);
+            }
+            $api_conn->queue_bin_msg(API_MSG_RESV_PUSH, $opts, $resv_info_tlv, @path_elem_tlvs);
+            $ct++;
+            $api_conn->send_bin_msg();
+            print %{$resv};
+            foreach my $elem (@{$resv->{path}}) {
+                print "\t";
+                print %$elem;
+                print "\n";
+            }
+        print  "\n";
+    }
+    print "sleep $interval seconds\n";
+    sleep($interval);
+}
+
+1;
