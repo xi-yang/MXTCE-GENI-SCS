3bbdb9b1c7d781ad8041a4ca2d82f6d72a2cd20e
xi <xi@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2014-08-15 20:24:16 +0000

    mpvb_actions work in progress
    git-svn-id: file:///var/www/svn/mxtce-sw/trunk/mxtce-sw@651 52860be4-34ab-47e5-b986-9ec2afffd7f9

--- src/compute/mpvb_actions.hh
+++ src/compute/mpvb_actions.hh
@@ -55,4 +55,109 @@ class Action_ProcessRequestTopology_MPVB: public Action
         virtual void CleanUp();
 };
 
+class Action_PrestageCompute_MPVB: public Action
+{
+    protected:
+    
+    public:
+        Action_PrestageCompute_MPVB(): Action(){ }
+        Action_PrestageCompute_MPVB(string& n, ComputeWorker* w): Action(n, w) { }
+        virtual ~Action_PrestageCompute_MPVB() { }
+    
+        virtual void Process();
+        virtual bool ProcessChildren();
+        virtual bool ProcessMessages();
+        virtual void Finish();
+        virtual void CleanUp();
+};
+
+
+class Action_ComputeServiceTopology_MPVB: public Action
+{
+    protected:
+    
+    public:
+        Action_ComputeServiceTopology_MPVB(): Action(){ }
+        Action_ComputeServiceTopology_MPVB(string& n, ComputeWorker* w): Action(n, w) { }
+        virtual ~Action_ComputeServiceTopology_MPVB() { }
+    
+        virtual void Process();
+        virtual bool ProcessChildren();
+        virtual bool ProcessMessages();
+        virtual void Finish();
+        virtual void CleanUp();
+};
+
+
+class Action_FinalizeServiceTopology_MPVB: public Action
+{
+    protected:
+    
+    public:
+        Action_FinalizeServiceTopology_MPVB(): Action(){ }
+        Action_FinalizeServiceTopology_MPVB(string& n, ComputeWorker* w): Action(n, w) { }
+        virtual ~Action_FinalizeServiceTopology_MPVB() { }
+    
+        virtual void Process();
+        virtual bool ProcessChildren();
+        virtual bool ProcessMessages();
+        virtual void Finish();
+        virtual void CleanUp();
+};
+
+#define MPVB_TYPE_T 1
+#define MPVB_TYPE_P 2
+#define MPVB_TYPE_B 3
+
+
+class KSPCache {
+private:
+    map<string, map<string, list<TPath*>*>*> cacheMap;
+public:
+    ~KSPCache() {
+        map<string, map<string, list<TPath*>*>*>::iterator itM = cacheMap.begin();
+        for (; itM != cacheMap.end(); itM++) {
+            map<string, list<TPath*>*>* entry = itM->second;
+            map<string, list<TPath*>*>::iterator itM2 = entry->begin();
+            for (; itM2 != entry->end(); itM2++) {
+                // delete KSP
+                list<TPath*>* ksp = itM2->second;
+                list<TPath*>::iterator itL = ksp->begin();
+                for (; itL != ksp->end(); itL++) {
+                    delete (*itL);
+                }
+                delete ksp;
+            }
+            delete entry;
+        }
+    }
+
+    void Add(TNode* srcNode, TNode* dstNode, list<TPath*>* ksp) {
+        return this->Add(srcNode->GetName(), dstNode->GetName(), ksp);
+    }
+    
+    void Add(string src, string dst, list<TPath*>* ksp) {
+        if (cacheMap.find(src == cacheMap.end()) {
+            cacheMap.put(src, new map<string, list<TPath*>*>*);            
+        }
+        map<string, list<TPath*>*>* entry = (map<string, list<TPath*>*>*)cacheMap[src];
+        if (entry->find(dst == cacheMap.end()) {
+            entry->put(dst, ksp);
+        }
+    }
+
+    list<TPath*>* Lookup(TNode* srcNode, TNode* dstNode) {
+        return Lookup(srcNode->GetName(), dstNode->GetName());
+    }
+    
+    list<TPath*>* Lookup(string stc, string dst) {
+        if (cacheMap.find(src) == cacheMap.end()) 
+            return NULL;
+        map<string, list<TPath*>*>* entry = (map<string, list<TPath*>*>*)cacheMap[src];
+        if (entry->find(dst == cacheMap.end()) 
+            return NULL;
+        return (*entry)[dst];
+    }            
+};
+
 #endif
