4b3acc920d09900a8ee20923a2f923c2cf348589
xi <xi@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2014-10-21 18:00:31 +0000

    bug fix: workflow dependency undetermined between equal non-translation hops
    git-svn-id: file:///var/www/svn/mxtce-sw/trunk/mxtce-sw@736 52860be4-34ab-47e5-b986-9ec2afffd7f9

--- src/geni/workflow.cc
+++ src/geni/workflow.cc
@@ -245,7 +245,7 @@ void WorkflowData::ComputeDependency()
                 if (!loop_d1_d2 
                     && (numSameDomainHops1 < numSameDomainHops2
                         || (numSameDomainHops1 == numSameDomainHops2 && 
-                        availableVlanRange1.Size() > availableVlanRange2.Size())))
+                        availableVlanRange1.Size() >= availableVlanRange2.Size())))
                 {
                     D1->GetLowers().push_back(D2);
                     D2->GetUppers().push_back(D1);
@@ -255,7 +255,7 @@ void WorkflowData::ComputeDependency()
                 else if (!loop_d2_d1 
                     && (numSameDomainHops1 > numSameDomainHops2
                         || (numSameDomainHops1 == numSameDomainHops2 &&
-                        availableVlanRange1.Size() < availableVlanRange2.Size())))
+                        availableVlanRange1.Size() <= availableVlanRange2.Size())))
                 {
                     D2->GetLowers().push_back(D1);
                     D1->GetUppers().push_back(D2);
