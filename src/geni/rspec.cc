6737cbac8794e31be41535df0eb5d9801feb6f68
xi <xi@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2014-10-21 15:16:40 +0000

    bug fix
    git-svn-id: file:///var/www/svn/mxtce-sw/branches/geni-scs-r2.0@735 52860be4-34ab-47e5-b986-9ec2afffd7f9

--- src/geni/rspec.cc
+++ src/geni/rspec.cc
@@ -1228,6 +1228,7 @@ Message* GeniRequestRSpec::CreateApiRequestMessage(map<string, xmlrpc_c::value>&
                         {
                             break;
                         }
+                        /*
                         else if (strncasecmp((const char*)xmlIfNode->name, "link_type", 16) == 0)
                         {
                             xmlChar* pBuf = xmlNodeGetContent(xmlIfNode);
@@ -1236,6 +1237,7 @@ Message* GeniRequestRSpec::CreateApiRequestMessage(map<string, xmlrpc_c::value>&
                             if (linkTypeValue.compare("vlan") != 0)
                                 break;
                         }
+                        */
                     }
                 }
                 // skip shared_vlan links
