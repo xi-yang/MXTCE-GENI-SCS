57ebc1067bf8bcff2768ca820358d883d6af6c2d
xi <xi@52860be4-34ab-47e5-b986-9ec2afffd7f9>
2014-12-01 18:41:44 +0000

    bug fix: http://groups.geni.net/geni/ticket/1349
    git-svn-id: file:///var/www/svn/mxtce-sw/trunk@743 52860be4-34ab-47e5-b986-9ec2afffd7f9

--- mxtce-sw/src/compute/compute_actions.cc
+++ mxtce-sw/src/compute/compute_actions.cc
@@ -1006,8 +1006,16 @@ void Action_ComputeSchedulesWithKSP::Process()
             for (; itH != this->_userConstraint->getHopExclusionList()->end(); itH++)
             {
                 string& hopUrn = *itH;
-                tewg->PruneByExclusionUrn(hopUrn);
-            }
+                size_t delim = hopUrn.find("=");
+                if (delim == string::npos) // excluding hop link(s)
+                    tewg->PruneByExclusionUrn(hopUrn);
+                else // excluding vlans on the hop link(s)
+                {
+                    string aUrn = hopUrn.substr(0, delim);
+                    string vlanRange = hopUrn.substr(delim+1);
+                    tewg->PruneHopVlans(aUrn, vlanRange);
+                }
+            }        
         }
         // applying reservation data
         time_t startTime = (*itT);
